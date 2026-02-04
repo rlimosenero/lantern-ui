86baafee88b363697064a8023804268d349e3aa3
rey.limosenero <rey.limosenero@gmail.com>
2026-02-04 10:23:56 +0800

    initial commit v 19

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/login', pathMatch: 'full'},
+    { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
     {
         path: 'login',
         component: LoginComponent
