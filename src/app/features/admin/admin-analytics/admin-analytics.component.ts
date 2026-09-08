4cc19a67d871a926d66fc38a488278b76cf04eee
Jesrey L. Suazo <jlsuazo@headoffice.corp.bpi.com.ph>
2026-09-08 08:40:00 +0800

    WIP: analytics page

--- src/app/features/admin/admin-analytics/admin-analytics.component.ts
+++ src/app/features/admin/admin-analytics/admin-analytics.component.ts
@@ -1,39 +1,120 @@
-import { Component, inject, computed } from '@angular/core';
+import { Component, inject, computed, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
-import { MonitoringService } from '../services/monitoring.service';
+import { KeyValuePipe } from '@angular/common';
+import { LoaderComponent } from '../../../shared/components/loader/loader.component';
+import { AdminAnalyticsService } from './admin-analytics.service';
 
 @Component({
   selector: 'app-admin-analytics',
   standalone: true,
-  imports: [CommonModule, MatGridListModule, MatCardModule, MatIconModule],
+  imports: [
+    CommonModule,
+    MatGridListModule,
+    MatCardModule,
+    MatIconModule,
+    KeyValuePipe,
+    LoaderComponent
+  ],
   templateUrl: './admin-analytics.component.html',
   styleUrl: './admin-analytics.component.scss'
 })
-export class AdminAnalyticsComponent {
-  private monitoringService = inject(MonitoringService);
+export class AdminAnalyticsComponent implements OnInit {
+  activeTab: 'applications' | 'webServices' = 'applications';
+  isCompletenessLoading = false;
 
-  // Map service signals to local display objects
-  stats = computed(() => [
-    { 
-      title: 'Total Services', 
-      value: this.monitoringService.totalCount(), 
-      icon: 'hub', 
-      color: '#3f51b5' 
+  summaryData = {
+    // "totalApplications": 10,
+    // "overallCompleteness": 78.00,
+    "applicationsAddedThisMonth": 2,
+    "completenessChangeVsLastMonth": 4.20,
+    "fullyComplete": 4,
+    "needsAttention": 3
+  }
+
+  applications = [
+    {
+      name: 'User Authentication API',
+      overallCompletenessRate: 100,
+      mandatoryCompletenessRate: 100,
+      status: 'COMPLETE'
     },
-    { 
-      title: 'Systems Online', 
-      value: this.monitoringService.onlineCount(), 
-      icon: 'check_circle', 
-      color: '#2e7d32' 
-    },
-    { 
-      title: 'Critical Alerts', 
-      value: this.monitoringService.alertCount(), 
-      icon: 'report_problem', 
-      color: '#d32f2f' 
+    {
+      name: 'Payment Gateway',
+      overallCompletenessRate: 93.33,
+      mandatoryCompletenessRate: 80,
+      status: 'PARTIAL'
     }
-  ]);
+  ];
+
+  webServices = [
+    {
+      name: 'User Service',
+      overallCompletenessRate: 95,
+      mandatoryCompletenessRate: 90,
+      status: 'COMPLETE'
+    },
+    {
+      name: 'Payment Service',
+      overallCompletenessRate: 72,
+      mandatoryCompletenessRate: 65,
+      status: 'PARTIAL'
+    }
+  ];
+
+  constructor(
+    private adminAnalyticsService: AdminAnalyticsService,
+  ) { }
+
+  ngOnInit() {
+    this.loadCompletenessData();
+    this.loadApplicationList();
+  }
+
+  formatLabel(key: string): string {
+    return key
+      .replace(/([A-Z])/g, ' $1')
+      .replace(/^./, char => char.toUpperCase());
+  }
+
+  switchTab(tab: 'applications' | 'webServices'): void {
+    if (this.activeTab === tab) {
+      return;
+    }
+
+    this.activeTab = tab;
+  }
+
+  getActiveCompletenessData() {
+    return this.activeTab === 'applications'
+      ? this.applications
+      : this.webServices;
+  }
+
+  loadCompletenessData() {
+    this.adminAnalyticsService.getCompletenessSummary().subscribe({
+      next: (res) => {
+        console.log('summary')
+        console.log(res);
+      },
+      error: (err) => {
+        console.error('Error fetching completeness summary:', err);
+      }
+    });
+  }
+
+  loadApplicationList() {
+    this.adminAnalyticsService.getApplicationCompleteness().subscribe({
+      next: (res) => {
+        console.log('applicationlist')
+        console.log(res)
+      },
+      error: (err) => {
+        console.error('Error fetching application completeness:', err);
+      }
+    });
+  }
+
 }
---
+++ src/app/features/admin/admin-analytics/admin-analytics.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+
+import { APP_CONFIG } from '../../../core/models/app.config.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AdminAnalyticsService {
+
+  private http = inject(HttpClient);
+  private config = inject(APP_CONFIG);
+
+  private readonly baseUrl =
+    `${this.config.baseUrl}/analytics/completeness`;
+
+  getCompletenessSummary() {
+    const url = `${this.baseUrl}/summary`;
+
+    return this.http.get(url);
+  }
+
+  getApplicationCompleteness(page: number = 0, size: number = 10) {
+    const url = `${this.baseUrl}/applications?page=${page}&size=${size}`;
+
+    return this.http.get(url);
+  }
+
+}
